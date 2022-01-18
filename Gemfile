329827b518df0326c95e19a4a3bb8f5e0b37d422
dawidl022 <domdaw7@gmail.com>
2022-01-18 09:09:59 +0000

    Add users index pagination

--- Gemfile
+++ Gemfile
@@ -3,12 +3,15 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 gem 'rails',      '6.1.4.1'
 gem 'bcrypt',     '3.1.13'
+gem 'faker',      '2.11.0'
 gem 'puma',       '5.3.1'
 gem 'sass-rails', '6.0.0'
 gem 'webpacker',  '5.4.0'
 gem 'turbolinks', '5.2.1'
 gem 'jbuilder',   '2.10.0'
 gem 'bootstrap-sass', '3.4.1'
+gem 'will_paginate',  '3.3.0'
+gem 'bootstrap-will_paginate', '1.0.0'
 gem 'rexml', '~> 3.2', '>= 3.2.4'
 gem 'bootsnap',   '1.7.2', require: false
 
---
+++ test/integration/users_index_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class UsersIndexTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
+  test "index including pagination" do
+    log_in_as(@user)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination', count: 2
+
+    User.paginate(page: 1).each do |user|
+      assert_select 'a[href=?]', user_path(user), text: user.name
+    end
+
+    get users_path, params: { page: 2 }
+    User.paginate(page: 2).each do |user|
+      assert_select 'a[href=?]', user_path(user), text: user.name
+    end
+  end
+end
