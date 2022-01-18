a0c1420980785d1de8c072173ace234b1b6a56c0
dawidl022 <domdaw7@gmail.com>
2022-01-18 07:39:39 +0000

    Add users index

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -40,4 +40,15 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert flash.empty?
     assert_redirected_to root_url
   end
+
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+
+  test "should show users index when logged in" do
+    log_in_as(@user)
+    get users_path
+    assert_template "users/index"
+  end
 end
