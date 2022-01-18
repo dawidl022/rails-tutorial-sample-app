e8157fc38c29e30983922e3ab3520168cf6d071e
dawidl022 <domdaw7@gmail.com>
2022-01-18 07:06:55 +0000

    Add page authorisation and friendly redirects

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -5,20 +5,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
-  test "unsuccessful edit" do
-    get edit_user_path(@user)
-    assert_template 'users/edit'
-    patch user_path(@user), params: { user: {
-      name: "", email: "foo_at_invalid", password: "foo",
-      password_confirmation: "bar"
-    } }
-    assert_template 'users/edit'
-    assert_select "div.alert", /4/
-  end
-
-  test "successful edit" do
-    get edit_user_path(@user)
-    assert_template 'users/edit'
+  def update_user_assertions
     name = "Foo Bar"
     email = "foo@bar.com"
 
@@ -31,5 +18,39 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user.reload
     assert_equal name, @user.name
     assert_equal email, @user.email
+  end
+
+  test "unsuccessful edit" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: {
+      name: "", email: "foo_at_invalid", password: "foo",
+      password_confirmation: "bar"
+    } }
+    assert_template 'users/edit'
+    assert_select "div.alert", /4/
+  end
+
+  test "successful edit" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    update_user_assertions
+  end
+
+  test "successful edit with friendly forwarding" do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+    update_user_assertions
+  end
+
+  test "friendly forwarding only redirects once" do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+    log_in_as(@user)
+    assert_redirected_to user_url(@user)
   end
 end
