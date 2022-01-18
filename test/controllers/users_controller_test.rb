e4c6558ca6039c58a0eec9fa5430093c8491f0a6
dawidl022 <domdaw7@gmail.com>
2022-01-18 11:37:56 +0000

    Add admin users and user deletion

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -51,4 +51,26 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get users_path
     assert_template "users/index"
   end
+
+  test "should not be able to update admin status through patch request" do
+    log_in_as(@another_user)
+    assert_not @another_user.admin?
+    patch user_path(@another_user), params: { user: { admin: true }}
+    assert_not @another_user.reload.admin?
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@another_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
 end
